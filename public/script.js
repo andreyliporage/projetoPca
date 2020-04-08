833a2d3b4d1bf91830978214c97b8517dbbf2cb7
andreyliporage <andreyliporage@gmail.com>
2020-04-08 18:29:25 -0300

    update

--- public/script.js
+++ public/script.js
@@ -7,17 +7,19 @@ for (item of menuItems) {
     }
 }
 
-console.log(currentPage)
+const modalOverlay = document.querySelector(".modal-overlay")
+const cards = document.querySelectorAll(".card")
 
-// const modalOverlay = document.querySelector(".modal-overlay")
-// const cards = document.querySelectorAll(".card")
+for (let card of cards) {
+    card.addEventListener("click", function() {
+        const videoId = card.getAttribute("id")
+        modalOverlay.classList.add("active")
+        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`
 
-// for (let card of cards) {
-//     card.addEventListener("click", function() {
-//         modalOverlay.classList.add("active")
-//     })
-// }
+    })
+}
 
-// document.querySelector(".close-modal").addEventListener("click", function() {
-//     modalOverlay.classList.remove("active")
-// }) 
+document.querySelector(".close-modal").addEventListener("click", function() {
+    modalOverlay.classList.remove("active")
+    modalOverlay.querySelector("iframe").src = ""
+}) 
